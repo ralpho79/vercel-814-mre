2e24436f04f2f4d239b26acd883acd5019258f95
ralpho79 <101933657+ralpho79@users.noreply.github.com>
2023-02-09 19:28:43 +0100

    Update index.py

--- api/index.py
+++ api/index.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 def index():
     if request.method == "POST":
         text = request.form["input_text"]
-        list_to_csv(text,"/tmp/example.csv")
+        list_to_csv(text,"tmp/example.csv")
         return redirect(url_for("index"))
     
     my_list=csv_to_list("tmp/example.csv")
